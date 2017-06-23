612815654d5c84d1ca87baf7d3a0169f942351ad
Nick Scott <nicholai.scott@gmail.com>
2017-06-23 16:40:21 +1200

    Added sounds to rocket, no rocket trail though

--- Assets/MissileController.cs
+++ Assets/MissileController.cs
@@ -5,83 +5,92 @@ using UnityEngine;
 public class MissileController : MonoBehaviour {
 
 	private bool armed = false;
-    private bool exploded = false;
+	private bool exploded = false;
 	Vector2 destination = new Vector2(0, 100);
 	float missileSpeed = 100f;
-    ParticleSystem smoke;
-    ParticleSystem fire;
-    ParticleSystem explosion;
-    float missileExplodedTime = 0;
-    int missileTimeoutAfterExplosion = 5;
+	ParticleSystem smoke;
+	ParticleSystem fire;
+	ParticleSystem explosion;
+	float missileExplodedTime = 0;
+	int missileTimeoutAfterExplosion = 5;
+	AudioSource _audio;
 
-    // Use this for initialization
-    void Start () {
-        smoke = transform.Find("SmokeEffect").GetComponent<ParticleSystem>();
-        fire = transform.Find("FlamesParticleEffect").GetComponent<ParticleSystem>();
-        explosion = transform.Find("BigExplosionEffect").GetComponent<ParticleSystem>();
-        smoke.Play(true);
-        fire.Play(true);
-        explosion.Stop(true);
+	// Use this for initialization
+	void Start () {
+		smoke = transform.Find("SmokeEffect").GetComponent<ParticleSystem>();
+		fire = transform.Find("FlamesParticleEffect").GetComponent<ParticleSystem>();
+		explosion = transform.Find("BigExplosionEffect").GetComponent<ParticleSystem>();
+		smoke.Play(true);
+		fire.Play(true);
+		explosion.Stop(true);
+		_audio = transform.GetComponent<AudioSource>();        
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (armed)
-        {
-            Debug.Log("Distance to target: " + Vector2.Distance(transform.position, destination));
-            if (Vector2.Distance(transform.position, destination) < 5)
+		{
+			//Debug.Log("Distance to target: " + Vector2.Distance(transform.position, destination));
+			if (Vector2.Distance(transform.position, destination) < 5)
 			{
-                explode();
+				explode();
 			}
 			float step = missileSpeed * Time.deltaTime;
 			transform.position = Vector2.MoveTowards(transform.position, destination, step);
 		}
-        if(exploded && Time.time - missileExplodedTime > missileTimeoutAfterExplosion)
-        {
-            Debug.Log("Cleaning up missile");
-            Destroy(transform.gameObject);
-        }
+		if(exploded && Time.time - missileExplodedTime > missileTimeoutAfterExplosion)
+		{
+			Debug.Log("Cleaning up missile");
+			Destroy(transform.gameObject);
+		}
 	}
 
-    public void launch(Vector2 origin, Vector2 destination)
-    {
-        Debug.Log("Misile launched from: " + origin + " to: " + destination);
-        this.destination = destination;
-        transform.position = origin;
-        arm();
-    }
+	public void launch(Vector2 origin, Vector2 destination)
+	{
+		Debug.Log("Misile launched from: " + origin + " to: " + destination);
+		this.destination = destination;
+		transform.position = origin;
+		arm();
+	}
 
-    private void explode()
-    {
-        smoke.Stop(true);
-        fire.Stop(true);
-        explosion.Play(true);
-        armed = false;
-        exploded = true;
-        missileExplodedTime = Time.time;
-        Debug.Log("Missile Armed: " + armed);
-    }
+	private void explode()
+	{
+		smoke.Stop(true);
+		fire.Stop(true);
+		explosion.Play(true);
+		armed = false;
+		exploded = true;
+		missileExplodedTime = Time.time;
+		_audio.Stop();
+		Object[] missileExplosion = Resources.LoadAll("Sounds/PlayerMissile/Explosion", typeof(AudioClip));
+		_audio.clip = (AudioClip)missileExplosion[Random.Range(0, missileExplosion.Length)];
+        _audio.PlayDelayed(0.5f);
+		Debug.Log("Missile Armed: " + armed);
+	}
 
-    private void arm()
-    {
-        if (smoke)
-        {
-            smoke.Play(true);
-        }
-        if (fire)
-        {
-            fire.Play(true);
-        }
-        if (explosion)
-        {
-            explosion.Stop(true);
-        }
-        armed = true;
-        Debug.Log("Missile Armed: " + armed);
-    }
+	private void arm()
+	{
+		if (smoke)
+		{
+			smoke.Play(true);
+		}
+		if (fire)
+		{
+			fire.Play(true);
+		}
+		if (explosion)
+		{
+			explosion.Stop(true);
+		}
+		armed = true;
+		Debug.Log("Missile Armed: " + armed);
+		AudioSource audio = transform.GetComponent<AudioSource>();
+		Object[] missileLaunch = Resources.LoadAll("Sounds/PlayerMissile/Launch", typeof(AudioClip));
+		audio.PlayOneShot((AudioClip)missileLaunch[Random.Range(0, missileLaunch.Length)], 0.5f);
+	}
 
-    public bool hasExploded()
-    {
-        return exploded;
-    }
+	public bool hasExploded()
+	{
+		return exploded;
+	}
 }
--- Assets/TouchController.cs
+++ Assets/TouchController.cs
@@ -6,15 +6,15 @@ public class TouchController: MonoBehaviour {
 
 
     SiloController silo;
-    Camera camera;
+    //Camera camera;
     // Use this for initialization
     void Start () {
         silo = new SiloController();
         //Screen.SetResolution(Screen.width, Screen.height, true);
-        camera = this.GetComponent<Camera>();
+        //camera = this.GetComponent<Camera>();
         //float aspectRatio = Screen.width / Screen.height;
         Debug.Log("Width: " + Screen.width + " Height: " + Screen.height);
-        camera.projectionMatrix = Matrix4x4.Ortho(0, Screen.width, 0, Screen.height, 0.3f, 1000f);
+        this.GetComponent<Camera>().projectionMatrix = Matrix4x4.Ortho(0, Screen.width, 0, Screen.height, 0.3f, 1000f);
         
     }
 
