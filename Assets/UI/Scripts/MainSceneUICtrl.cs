6b7d61124ffd6626f2cda186d78a521388e2b938
stevenpeck <stevepeck@hotmail.co.nz>
2017-06-25 14:15:32 +1200

    Added Ingame UI
    Added Ingame UI along with scripts for navigation, pausing of game and score text.
    
    Issue: While paused game SFX continue and on resume rockets still launch to click/touch locations.
    Have paused the game using Time.timeScale might need to try find a different method of pausing?

---
+++ Assets/UI/Scripts/MainSceneUICtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainSceneUICtrl : MonoBehaviour {
+
+	public bool isPaused;
+	public GameObject pauseMenuCanvas;
+	public GameObject inGameCanvas;
+
+	// Update is called once per frame
+	void Update () {
+		if(isPaused){
+			pauseMenuCanvas.SetActive (true);
+			Time.timeScale = 0f;
+		}
+		else {
+			pauseMenuCanvas.SetActive (false);
+			Time.timeScale = 1f;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			isPaused = !isPaused;
+		}
+	}
+
+	public void Pause(){
+		isPaused = true;
+		inGameCanvas.SetActive (false);
+	}
+
+	public void Resume(){
+		isPaused = false;
+		inGameCanvas.SetActive (true);
+	}
+
+	public void Settings(){
+
+	}
+
+	public void Quit(string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+}
---
+++ Assets/UI/Scripts/ScoreText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Stores and updates the players score
+/// </summary>
+
+public class ScoreText : MonoBehaviour {
+
+	public Text textInstance;
+	private int scoreLegnth = 4;
+	private int score = 0;
+	private int numOfZeros;
+	private string newScore;
+
+	public string ScoreString{
+		get{return "Score: " + newScore;}
+	}
+
+	// Use this for initialization
+	void Start () {
+		textInstance = GetComponent<Text> ();
+		//Test Add
+		AddToScore (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textInstance.text = ScoreString;
+	}
+
+	//Adds int value to score
+	public void AddToScore(int i){
+		score += i;
+		numOfZeros = scoreLegnth - score.ToString().Length;
+		for(int j = 0; j < numOfZeros; j++){
+			newScore += "0";
+		}
+		newScore += score.ToString ();
+	}
+}
