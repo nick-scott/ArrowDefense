1bee6bc88ad19627c065a0c4262f7d0332605930
Nick Scott <nicholai.scott@gmail.com>
2017-06-27 15:26:16 +1200

    added utilities for positioning objects so its not super awkward

--- Assets/EnvorinmentBuilder.cs
+++ Assets/EnvorinmentBuilder.cs
@@ -1,30 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class EnvorinmentBuilder : MonoBehaviour{
-    GameObject hill;
-    GameObject groundFill;
-    GameObject tree;
+	GameObject hill;
+	GameObject groundFill;
+	GameObject tree;
 	// Use this for initialization
 	static float groundHeightInPercent = 10f;
 	float realPercentage = 100 / groundHeightInPercent;
 
 	void Start () {
-		transform.position = new Vector3(Screen.width / 2, Screen.height / 2, 100);
-        hill = transform.Find("Hill").gameObject;
-        groundFill = transform.Find("GroundFill").gameObject;
-        drawGround();
+		transform.position = new Vector3(UnitUtility.centerWidth(), UnitUtility.centerHeight(), 100);
+		hill = transform.Find("Hill").gameObject;
+		groundFill = transform.Find("GroundFill").gameObject;
+		drawGround();
 		Debug.Log("groundHeightInPercent: " + groundHeightInPercent);
 	}
 
 	private void drawGround()
-    {
-        hill.transform.position = new Vector3(Screen.width/2, (Screen.height / realPercentage)/2, transform.position.z);
-        groundFill.transform.position = new Vector3(Screen.width/2, (Screen.height / realPercentage)/4, transform.position.z);
-        hill.transform.localScale = new Vector3(Screen.width*1.4f, Screen.height / realPercentage/2, 1);
-        groundFill.transform.localScale = new Vector3(Screen.width,( Screen.height / realPercentage)/2, 1);
-        Debug.Log("groundHeight: " + Screen.height / realPercentage);
+	{
+		hill.transform.position = new Vector3(UnitUtility.centerWidth(), UnitUtility.percentHeight(groundHeightInPercent)/2, transform.position.z);
+		groundFill.transform.position = new Vector3(UnitUtility.centerWidth(), UnitUtility.percentHeight(groundHeightInPercent) / 4, transform.position.z);
+		hill.transform.localScale = new Vector3(Screen.width*1.4f, UnitUtility.percentHeight(groundHeightInPercent), 1);
+		groundFill.transform.localScale = new Vector3(Screen.width, UnitUtility.percentHeight(groundHeightInPercent) / 2, 1);
+		Debug.Log("groundHeight: " + Screen.height / realPercentage);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/UnitUtility.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnitUtility {
+
+	public static float percentHeight(float percent)
+	{
+		float realPercentage = 100 / percent;
+		return Screen.height/realPercentage;
+	}
+
+	public static float percentWidth(float percent)
+	{
+		float realPercentage = 100 / percent;
+		return Screen.width / realPercentage;
+	}
+
+	public static float centerWidth()
+	{
+		return Screen.width / 2;
+	}
+
+	public static float centerHeight()
+	{
+		return Screen.height / 2;
+	}
+
+    public static float byAnchorPercentHeight(GameObject anchorObject, Anchor anchor, float percent)
+    {
+        switch (anchor)
+        {
+            case Anchor.TOP:
+                return 0;
+            case Anchor.BOTTOM:
+                return 0;
+        }
+        return percentHeight(percent);
+    }
+
+    public static float byAnchorPercentWidth(GameObject anchorObject, Anchor anchor, float percent)
+    {
+        switch (anchor)
+        {
+            case Anchor.LEFT:
+                return 0;
+            case Anchor.RIGHT:
+                return 0;
+        }
+        return percentHeight(percent);
+    }
+
+    public enum Anchor{
+        TOP,
+        RIGHT,
+        BOTTOM,
+        LEFT
+    }
+    
+}
